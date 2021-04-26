f4ccc6e6258fb0dc5cde48d16008534e8a9b6df6
frozen90 <pych60@gmail.com>
2021-04-26 20:38:30 +0100

    Initial modeling on the products.

--- backend/backendapp/admin.py
+++ backend/backendapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Staff
+from .models import Staff, Supplier, Product, ProductGroup, Brand
 # Register your models here.
 
 admin.site.register(Staff)
+admin.site.register(Supplier)
+admin.site.register(Product)
+admin.site.register(ProductGroup)
+admin.site.register(Brand)
--- backend/backendapp/models.py
+++ backend/backendapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 # Create your models here.
 
 class Staff(models.Model):
@@ -9,3 +10,50 @@ class Staff(models.Model):
 
     def __str__(self):
         return self.firstName + ' ' + self.secondName
+
+class Supplier(models.Model):
+    supplierName = models.CharField(max_length=50)
+    suppilerAddress = models.CharField(max_length=100)
+    supplierPostcode = models.CharField(max_length=7)
+    supplierEmail = models.EmailField()
+    supplierContactName = models.CharField(max_length=50)
+    supplierCode = models.CharField(max_length=5)
+    supplierCountry = CountryField()
+    def __str__(self):
+        return self.supplierName
+
+class ProductGroup(models.Model):
+    groupName = models.CharField(max_length=200)
+    description = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.groupName
+
+class Product(models.Model):
+    productName = models.CharField(max_length=200)
+    description = models.CharField(max_length=1024)
+    available = models.BooleanField(default=False)
+    barcode = models.CharField(max_length=52)
+    qty = models.IntegerField(default=0)
+    group = models.ForeignKey(ProductGroup, on_delete=models.PROTECT)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.productName
+
+
+class Brand(models.Model):
+    brandName = models.CharField(max_length=50)
+    description = models.CharField(max_length=1024)
+    brandSupplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.brandName
+
+
+
+
+
+
+
+
+    
