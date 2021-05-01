f5e1815e142842a94bb45721ea27f0af73c9b078
frozen90 <pych60@gmail.com>
2021-05-01 15:38:31 +0100

    Added handlers for product details page.@

--- backend/frontendapp/src/components/products/ProductsDashboard.js
+++ backend/frontendapp/src/components/products/ProductsDashboard.js
@@ -2,15 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Grid, Header, Segment, Divider,Menu, Container, } from 'semantic-ui-react';
 import { motion } from 'framer-motion';
-
+import ProductsDetails from './components/ProductsDetails';
+import ProductsGoodsIn from './components/ProductsGoodsIn';
+import ProductsStock from './components/ProductsStock';
+import ProductsStatistics from './components/ProductsStatistics';
 
 
 const ProductsDashboard = () =>{
     const dispatch = useDispatch();
-    const [activeItem, setActiveItem] = useState('Bio')
+    const [activeItem, setActiveItem] = useState('Details')
+    const [activeComponent, setActiveComponent] = useState(<ProductsDetails/>)
     const handleItemClick = (e) =>{
       setActiveItem(e.target.outerText)
-    }
+      if(e.target.outerText === 'Details'){
+        setActiveComponent(<ProductsDetails/>)
+      }else if(e.target.outerText === 'Stock'){
+        setActiveComponent(<ProductsStock/>)
+      }else if(e.target.outerText === 'Goods In'){
+        setActiveComponent(<ProductsGoodsIn/>)
+      }else if(e.target.outerText === 'Statistics'){
+        setActiveComponent(<ProductsStatistics/>)
+      }  
+      }
     return(
       
         <motion.div style={{backgroundColor:'#333',height:'100%'}} initial={{scale:0}} animate={{scale:1}} exit={{scale:0}} transition={{duration:0.5}}>
@@ -18,32 +31,31 @@ const ProductsDashboard = () =>{
         <Grid.Column width={4}>
           <Menu fluid inverted vertical >
             <Menu.Item
-              name='bio'
-              active={activeItem === 'Bio'}
+              name='Details'
+              active={activeItem === 'Details'}
               onClick={handleItemClick}
             />
             <Menu.Item
-              name='pics'
-              active={activeItem === 'Pics'}
+              name='Stock'
+              active={activeItem === 'Stock'}
               onClick={handleItemClick}
             />
             <Menu.Item
-              name='companies'
-              active={activeItem === 'Companies'}
+              name='Goods In'
+              active={activeItem === 'Goods In'}
               onClick={handleItemClick}
             />
             <Menu.Item
-              name='links'
-              active={activeItem === 'Links'}
+              name='Statistics'
+              active={activeItem === 'Statistics'}
               onClick={handleItemClick}
             />
           </Menu>
         </Grid.Column>
 
-        <Grid.Column stretched width={12} style={{height:'90vh'}}>
-          <Segment>
-            This is an stretched grid column. This segment will always match the
-            tab height
+        <Grid.Column stretched width={12} style={{height:'90vh',backgroundColor:'#333'}}>
+          <Segment inverted>
+              {activeComponent}
           </Segment>
         </Grid.Column>
       </Grid>
---
+++ backend/frontendapp/src/components/products/components/ProductsStatistics.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Grid, Header, Segment, Divider,Menu, Container, } from 'semantic-ui-react';
+import { motion } from 'framer-motion';
+
+
+
+export const ProductsStatistics = () =>{
+    
+    
+    return(
+        <motion.div initial={{x:-1000}} animate={{x:0}}>
+            Test Div
+        </motion.div>
+    )
+}
+
+export default ProductsStatistics;
