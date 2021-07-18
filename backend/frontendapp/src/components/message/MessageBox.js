07165b397b9c8cc1e9ac5c70b06ac479807c9ce2
frozen90 <pych60@gmail.com>
2021-07-18 21:25:32 +0100

    Fix vw.

--- backend/frontendapp/src/components/message/MessageBox.js
+++ backend/frontendapp/src/components/message/MessageBox.js
@@ -8,17 +8,16 @@ const MessageDiv = ({message}) => {
     const today = new Date()
     return (
       <motion.div initial={{opacity: 0}} animate={{ opacity: 1}} exit={{opacity:0}} style={{backgroundColor:'#3d3c39',marginTop:'5px'}}>
-        <Segment inverted>
-        <Comment.Group >
+        <Segment style={{ border:'none', boxShadow:'none'}} inverted>
+        <Comment.Group size='massive' >
           <Comment>
-           
             <Comment.Content>
             <Comment.Avatar src="../../static/img/avatarDef.svg" style={{height:'40px', width:'40px',marginRight:'5px', marginBottom:'10px', color:'white'}}/>
                 <Comment.Author as='a'  style={{color:'white'}}>frozen90</Comment.Author>
                 <Comment.Metadata  style={{color:'white'}}>
                 <div>{date.getDate() === today.getDate() && date.getMonth() === today.getMonth() && date.getFullYear() === today.getFullYear() ? 'Today at ' + date.toLocaleTimeString() :  date.toLocaleString()} </div>
               </Comment.Metadata >
-                <Comment.Text  style={{color:'white'}}>{message}</Comment.Text>
+                <Comment.Text  style={{color:'white', marginLeft:'45px', width:'65vw'}}>{message}</Comment.Text>
             </Comment.Content>
         </Comment>
       </Comment.Group>
